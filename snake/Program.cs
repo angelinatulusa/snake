1468d207f07e3050539f576e879ba5a07ffab11f
angelinatulusa <ang.tulusa@gmail.com>
2022-03-15 23:10:17 +0200

    vse

--- snake/Program.cs
+++ snake/Program.cs
@@ -2,48 +2,77 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace snake
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = Encoding.UTF8;
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+			Console.SetBufferSize(250, 80);
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+			Walls walls = new Walls(80, 25);
+			walls.Draw();
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
+			// Отрисовка точек			
+			Point p = new Point(4, 5, '*');
+			Snake snake = new Snake(p, 4, Direction.RIGHT);
+			snake.Draw();
 
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-            numList.RemoveAt(0);
+			FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+			Point food = foodCreator.CreateFood();
+			food.Draw();
 
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+			while (true)
+			{
+				if (walls.IsHit(snake) || snake.IsHitTail())
+				{
+					break;
+				}
+				if (snake.Eat(food))
+				{
+					food = foodCreator.CreateFood();
+					food.Draw();
+				}
+				else
+				{
+					snake.Move();
+				}
+
+				Thread.Sleep(100);
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
+			}
+			WriteGameOver();
+			Console.ReadLine();
+		}
 
 
+		static void WriteGameOver()
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition(xOffset, yOffset++);
+			WriteText("***************************", xOffset, yOffset++);
+			WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+			yOffset++;
+			WriteText("Автор: Ангелина Тулуша", xOffset + 2, yOffset++);
+			WriteText("Специально для GeekBrains", xOffset + 1, yOffset++);
+			WriteText("***************************", xOffset, yOffset++);
+		}
 
-            Console.ReadLine();
-        }
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
 
-        /*static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }*/
-    }
+	}
 }
